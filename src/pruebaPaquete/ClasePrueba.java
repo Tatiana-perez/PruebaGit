fdc7abc219023cd83c1dbfb0af5b79fe31940984
tatiana <tatianapoza@gmail.com>
2023-01-23 16:23:27 +0100

    Comienzo proyecto

---
+++ src/pruebaPaquete/ClasePrueba.java
@@ -0,0 +1,14 @@
+package pruebaPaquete;
+
+public class ClasePrueba {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		int y = 3;
+
+		x %=y;
+		System.out.println(y);
+	}
+
+}
---
+++ src/pruebaPaquete/U1_92_Pares.java
@@ -0,0 +1,15 @@
+package pruebaPaquete;
+
+import java.util.Scanner;
+
+public class U1_92_Pares {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 19; i >= 20; i--) {
+			if(i%2==0)
+			System.out.println(i + " ");
+		}
+	}
+
+}
